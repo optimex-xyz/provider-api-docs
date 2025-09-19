f0586779b4bf55277f53f1971eee5a4725944e01
Dung1312091 <quangdung100194@gmail.com>
2025-09-19 14:26:42 +0700

    Fix trade status

--- example/src/containers/swap-detail/MinAmount.tsx
+++ example/src/containers/swap-detail/MinAmount.tsx
@@ -3,11 +3,17 @@ import type { ISwapDetail } from "../../services/type";
 import { Block, IconLabel } from "../../components";
 import { HandCoins } from "lucide-react";
 import { SkeletonWrapper } from "../../components/SkeletonWrapper";
+import { ethers } from "ethers";
+import { useFindToken } from "../../hooks";
 
 interface MinAmountProps {
   data: ISwapDetail;
 }
 export const MinAmount = ({ data }: MinAmountProps) => {
+  const { data: toToken } = useFindToken(
+    data.to_token.token_id,
+    data.to_token.chain
+  );
   return (
     <Block className="flex w-full items-center justify-between">
       <IconLabel
@@ -15,7 +21,10 @@ export const MinAmount = ({ data }: MinAmountProps) => {
         label="Min. received amount"
       />
       <SkeletonWrapper isLoading={!data.min_amount_out}>
-        <p>{data.min_amount_out}</p>
+        <p>
+          {ethers.formatUnits(data.min_amount_out, toToken?.token_decimals)}{" "}
+          {toToken?.token_symbol}
+        </p>
       </SkeletonWrapper>
     </Block>
   );
